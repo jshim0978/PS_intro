c7b88f972554921a232b4cc3cea5adf23834649b
Jungwoo Shim <jshim0978@gmail.com>
2021-12-03 13:12:00 +0900

    testing

---
+++ RoadToFirstPage/1004.py
@@ -0,0 +1,17 @@
+import math
+
+if __name__ == '__main__':
+
+    testCase = int(input())
+
+    for i in range(testCase):
+        x1, y1, x2, y2, = map(int, input().split())
+        numberOfPlanets = int(input())
+
+        for j in range(numberOfPlanets):
+            cx, cy, r = map(int, input().split())
+
+    
+
+
+
